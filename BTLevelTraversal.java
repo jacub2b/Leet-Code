4102790cc14e26e0afb36adc28709abac26c00b7
jacub2b <yaakovsnett2@gmail.com>
2024-03-01 20:44:57 +0200

    solution - improved level order travesal algorithm

---
+++ BTLevelTraversal.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * @param
+ */
+class BTLevelTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null) return result;
+        Queue<TreeNode> nextLevelNodes = new LinkedList<TreeNode>();
+        Queue<TreeNode> currentLevelNodes = new LinkedList<TreeNode>();
+        List<Integer> currentLevelResult = new ArrayList<Integer>();
+
+        currentLevelNodes.add(root);
+
+        while(!currentLevelNodes.isEmpty()) {
+            while(!currentLevelNodes.isEmpty()) {
+                TreeNode current = currentLevelNodes.poll();
+                currentLevelResult.add(current.val);
+                if (current.left != null) nextLevelNodes.add(current.left);
+                if (current.right != null) nextLevelNodes.add(current.right);
+            }
+
+            result.add(currentLevelResult);
+            currentLevelNodes.addAll(nextLevelNodes);
+            nextLevelNodes = new LinkedList<TreeNode>();
+            currentLevelResult = new ArrayList<Integer>();
+        }
+
+        return result;
+    }
+}
